1d2d50d075316751f13c4086a957f0f4d90d7a81
Alen Karnil <akarnil@witekio.com>
2024-05-09 09:51:42 +0100

    python: keyboard interrupt handling

--- recipes-apps/iotc-python-demo/files/iotc-python-demo.py
+++ recipes-apps/iotc-python-demo/files/iotc-python-demo.py
@@ -6,15 +6,20 @@ import time
 import sys
 from model.json_device import JsonDevice
 
+from signal import signal, SIGINT
+keep_running = True
+def sigint_handler(signal_received, frame):
+    keep_running = False
+
 
 def main(argv):
     '''Main function'''
+    signal(SIGINT, sigint_handler)
     
     CREDENTIALS_PATH = argv[1:][0]
     device = JsonDevice(CREDENTIALS_PATH)
     device.connect()
-
-    while True:
+    while keep_running:
         if device.needs_exit and not device.in_ota:
             print("OTA requested exit, exiting")
             break
@@ -22,6 +27,7 @@ def main(argv):
         data_sent = device.send_device_states()
         print(data_sent)
         time.sleep(10)
+
 
 if __name__ == "__main__":
     main(sys.argv)
